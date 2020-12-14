df29139e19549badea1598d3054e0dc022d985cf
missxia <1065403523@qq.com>
2020-12-14 17:18:59 +0800

    Add files via upload

--- src/stromotion.py
+++ src/stromotion.py
@@ -2,44 +2,75 @@ import cv2
 import copy
 import numpy as np
 from yolact_src.eval import mian_init,evalimage,evalimagexia
+from hp_sort.tools_bsp import BspFit,bsp_for_kp2d
 from hp_sort.tools_op import load_npz,KpsToBboxes,op25b_sort_linspace
 from hp_sort.tools_sift import Stitcher,match_distance,EnhanceImage,distance_line,WarpPerspectiveMatrix,WarpPerspectiveMatrix
+PointDistance = 3000
 path = 'yolact_src/weights/yolact_base_54_800000.pth'
 net = mian_init(path)
 #移动视角Stromotion
 class StromotionCrossvideo():
-    def __init__(self,workspace,path_2dpose,path_video,extract=10):
+    def __init__(self,workspace,path_2dpose,path_video,extract=10,show_foot_track_line=True):
         self.extract = extract
+        self.show_foot_track_line = show_foot_track_line
         self.workspace = workspace
         self.path_2dpose = path_2dpose
         self.path_video = path_video
         self.stitcher = Stitcher()
-        self.out = cv2.VideoWriter(self.workspace+'StromtionCrossVideo.mp4',cv2.VideoWriter_fourcc(*'XVID'), 10, (1920,1080),True)
+        self.output = {'kp2d':{},'track':{}}
+        self.foot = None
+        self.out = cv2.VideoWriter(self.workspace+'StromtionCrossVideo.mp4',cv2.VideoWriter_fourcc(*'XVID'), 5, (1920,1080),True)
         self.main()
     def map_srcimage_pan(self,src,dst,i): 
         vis = self.stitcher.stitch(i,[src, dst], ratio=0.75,showMatches=False,reprojThresh=10.0)
         #cv2.imshow('vis',cv2.resize(vis,None,fx=0.2,fy=0.2))
-
-    def get_bbox(self):
+    def get_track(self):
+        list_track,list_frames = [],[]
+        pose1 = np.load(self.path_2dpose,allow_pickle=True )['op25b'].item()
+        for k1,v1 in pose1.items():
+            point = self.comput_distance_image(v1[0])
+            if point!=False:
+                list_track.append(point)
+                list_frames.append(k1)
+        return list_track,list_frames
+    def get_bbox(self,start_frame,end_frame):
         list_bboxs,frame=[],[]
         pose1 = np.load(self.path_2dpose,allow_pickle=True )['op25b'].item()
         #pose2 = np.load('D:/Dancepose/BuildSrcImage/camera3_2dpose.npz',allow_pickle=True)['op25b'].item()
         for k1,v1 in pose1.items():
             frame.append(k1)
             list_bboxs.append(v1[0])
+            
+            #kp2ds.append(v1[0])
         np_bbox = np.array(list_bboxs)
+        op2dpose = op25b_sort_linspace(np_bbox,frame,start_frame,end_frame)
         #print(np_bbox.shape)
-        bboxs = KpsToBboxes(np_bbox)
-        return bboxs,frame
+        bboxs = KpsToBboxes(op2dpose)
+        return bboxs,frame,list_bboxs
     def main(self):
         cap = cv2.VideoCapture(self.path_video)
         count = int(cap.get(7))
-        bboxs,frames = self.get_bbox()     
-        print(len(frames),len(bboxs))
-        list_bbox,list_borad_src,list_borad_dst=[],[],[]
+        start_frame,end_frame= 0,count
+        bboxs,frames,kp2ds = self.get_bbox(start_frame,end_frame)  
+        list_track,list_frames = self.get_track()
+        save_track=(int(list_track[0][0]),int(list_track[0][1]))
+        np_track = np.array(list_track)
+        np_track = np_track.reshape((np_track.shape[0],1,2))
+        bsp_track = bsp_for_kp2d(np_track,alpha=0.0001)#BSP数据
+        #print(len(frames),len(bboxs))
+        list_bbox,list_borad_src,list_borad_dst,list_zero=[],[],[],[]
         for i in range(count):
             ret,src_im = cap.read()
             if ret and i in frames:
+                if i < bsp_track.shape[0]:
+                    center = bsp_track[i]
+                    list_zero.append((int(center[0]),int(center[1])))
+                    save_track=(int(center[0]),int(center[1]))
+                    if self.show_foot_track_line :
+                        if len(list_zero)>1:
+                            for num in range(len(list_zero)-1):
+                                cv2.line(src_im,list_zero[num],list_zero[num+1],(0,0,255),3)
+                self.output['track'][frames.index(i)] = save_track
                 if i%self.extract ==0:
                    #print(dst_im.shape,src_im.shape)
                     #self.map_srcimage_pan(src_im,dst_im,i)
@@ -57,7 +88,7 @@ class StromotionCrossvideo():
                     #im_fcn = fcn_mask(fcn_im)
                     im_fcn = evalimagexia(net,fcn_im)
                     if isinstance(im_fcn,bool):
-                        print(i)
+                        #print(i)
                         continue 
                     re = np.zeros(fcn_im.shape,dtype='uint8')
                     re[:,:,0]=re[:,:,1]=re[:,:,2]=im_fcn
@@ -82,17 +113,67 @@ class StromotionCrossvideo():
 
                     list_borad_src.append(borad_src)
                     list_borad_dst.append(borad_dst)
-
+                    self.output['kp2d'][frames.index(i)] = kp2ds[frames.index(i)]
                     im = copy.deepcopy(src_im)
                     for n in range(len(list_borad_src)-1):
                         borad_src = list_borad_src[n]
                         borad_dst = list_borad_dst[n]
                         im = im*borad_dst
                         im = im+borad_src
-                    cv2.imshow('im',cv2.resize(im,None,fx=0.25,fy=0.25))
+                    cv2.imshow('im',cv2.resize(im,None,fx=0.5,fy=0.5))
                     self.out.write(im)
                     cv2.waitKey(1)
         cv2.imwrite((self.workspace+'StromtionCrossVideo.png'),im)
         self.out.release()
         cv2.waitKey(1000)
         cv2.destroyAllWindows()
+        
+    def comput_distance_image(self,pose,c=0.2):
+            point_foot_right,point_foot_left = [22,23,24],[19,20,21]
+            foot_left_compute,foot_right_compute=[],[]
+            for i in point_foot_right:
+                if 0 not in pose[i] and pose[i][2]>c:
+                    foot_right_compute.append(pose[i])
+            for i in point_foot_left:
+                if 0 not in pose[i] and pose[i][2]>c:
+                    foot_left_compute.append(pose[i])
+            if foot_right_compute!=[] or foot_left_compute!=[]:
+                pass
+            else:
+                return False
+            #找到存在并可信的脚
+            #取y值较大的脚
+            if len(foot_right_compute)!=0 and len(foot_left_compute)!=0:
+                np_foot_left_compute=np.array(foot_left_compute)
+                np_foot_right_compute=np.array(foot_right_compute)
+                y_left = np.max(np_foot_left_compute[:,1])
+                y_right = np.max(np_foot_right_compute[:,1])
+                x_left_mean,y_left_mean = np.mean(np_foot_left_compute[:,0]),np.mean(np_foot_left_compute[:,1])
+                x_right_mean,y_right_mean = np.mean(np_foot_right_compute[:,0]),np.mean(np_foot_right_compute[:,1])
+                if self.foot!=None:
+                    distance_left = distance_line([[x_left_mean,y_left_mean],self.foot])
+                    distance_right = distance_line([[x_right_mean,y_right_mean],self.foot])
+                    #像素y值最小且距离符合
+                    if max(y_left,y_right)==y_left and distance_left<PointDistance:
+                        x,y = np.mean(np_foot_left_compute[:,0]),np.mean(np_foot_left_compute[:,1])
+                    elif distance_right<PointDistance:
+                        x,y = np.mean(np_foot_right_compute[:,0]),np.mean(np_foot_right_compute[:,1]) 
+                    elif distance_right<distance_left:
+                        x,y = np.mean(np_foot_right_compute[:,0]),np.mean(np_foot_right_compute[:,1]) 
+                    else:
+                        x,y = np.mean(np_foot_left_compute[:,0]),np.mean(np_foot_left_compute[:,1])
+                else:
+                    if max(y_left,y_right)==y_left:
+                        x,y = np.mean(np_foot_left_compute[:,0]),np.mean(np_foot_left_compute[:,1])
+                        self.foot=[x,y]
+                    else:
+                        x,y = np.mean(np_foot_right_compute[:,0]),np.mean(np_foot_right_compute[:,1])
+                        self.foot=[x,y]
+            else:
+                if len(foot_right_compute)!=0:
+                    np_foot_right_compute=np.array(foot_right_compute)
+                    x,y = np.mean(np_foot_right_compute[:,0]),np.mean(np_foot_right_compute[:,1])
+                else:
+                    np_foot_left_compute=np.array(foot_left_compute)
+                    x,y = np.mean(np_foot_left_compute[:,0]),np.mean(np_foot_left_compute[:,1])
+            return (int(x),int(y))
